87165da2157455c08f2d9509d77907615bf9c444
Carlos Abramonte <car.abr.04@gmail.com>
2024-03-20 17:33:47 -0500

    Header global de la aplicacion terminado.

--- src/shared/components/HeaderGlobal.tsx
+++ src/shared/components/HeaderGlobal.tsx
@@ -6,13 +6,13 @@ const HeaderGlobal = () => {
   return (
     <div className="header-all">
       <img src={LogoRimac} alt="logo principal" className="header-all__logo" />
-      <label className="header-all__label">
+      <div className="header-all__label">
         <span className="header-all__label__buy">¡Compra por este medio!</span>
         <span className="material-symbols-outlined header-all__label__icon">
           call
         </span>
         <span  className="header-all__label__number">(01) 411 6001</span>
-      </label>
+      </div>
     </div>
   );
 };
